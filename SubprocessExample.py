6e2677e37b87693d74d2be9f040fef3da70e136d
PointAD <100715637+PointAD@users.noreply.github.com>
2022-08-04 21:13:23 +0430

    Update SubprocessExample.py

--- SubprocessExample.py
+++ SubprocessExample.py
@@ -1,7 +1,8 @@
 ##This Program Start Or Stop Services By Their Name Using Inputs From User
 while True : 
     import subprocess
-    ## Gets Input From User
+    ##                        |||||||||||||||||||    ||||||||||||||||||||||||||||||||
+    ## Gets Input From User   VVVVVVVVVVVVVVVVVVV    VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
     args = ['sc' , str(input("Start Or Stop : ")) , str(input("Enter Service Name : "))]
     result = subprocess.run(args)
     
